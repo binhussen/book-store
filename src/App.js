48742bcafa2c03e5a2ada7dd7aaefb7a99b285f4
binhussen <mhdhsna@gmail.com>
2022-09-14 16:48:58 +0300

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+/** @format */
+
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const Header = () => (
+  <header className="header">
+    <h1 className="title">BookStore CMS</h1>
+    <nav>
+      <ul className="navbar">
+        <li className="nav-item">BOOKS</li>
+        <li className="nav-item">CATAGORIES</li>
+        <li className="nav-item">User</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
