fc9b2d2dc78bc64622de017c81a4b942ba1075bd
binhussen <mhdhsna@gmail.com>
2022-09-14 16:49:42 +0300

    Add Books

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 import './App.css';
 import Header from './components/Header';
+import Books from './components/pages/Books';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Books />
     </div>
   );
 }
---
+++ src/components/pages/Books.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const Books = () => (
+  <ul>
+    <li>Book 1</li>
+    <li>Book 2</li>
+    <li>Book 3</li>
+  </ul>
+);
+
+export default Books;
