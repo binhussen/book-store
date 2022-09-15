7b276a87a3fdc8d3231d4d0f46c19ee7b30c412a
binhussen <mhdhsna@gmail.com>
2022-09-15 11:45:09 +0300

    Update Books to props

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,27 @@
+/**
+ * /* eslint-disable react/prop-types
+ *
+ * @format
+ */
+
 /** @format */
 
 import Action from './Action';
 import Progress from './Progress';
 
-const Book = () => (
-  <div className="p-8 bg-white rounded border border-w1 flex justify-between">
-    <div>
-      <p className="font-bold opacity-50">Action</p>
-      <h2 className="font-bold text-xl">The Hunger Games</h2>
-      <p className="text-sm font-light text-secondary">Suzanne Collins</p>
-      <Action />
-    </div>
-    <Progress />
-  </div>
-);
+const Book = ({ book }) => {
+	const { title, category, author } = book;
+	return (
+		<div className='p-8 bg-white rounded border border-w1 flex justify-between'>
+			<div>
+				<p className='font-bold opacity-50'>{category}</p>
+				<h2 className='font-bold text-xl'>{title}</h2>
+				<p className='text-sm font-light text-secondary'>{author}</p>
+				<Action />
+			</div>
+			<Progress />
+		</div>
+	);
+};
 
 export default Book;
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 /** @format */
 
 import AddBook from '../components/AddBook';
 import Book from '../components/Book';
 
-const Books = () => (
+const Books = ({ books }) => (
   <section className="p-[5%] bg-w2">
     <div className="flex flex-col gap-4">
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
     </div>
     <div className="h-0.5 w-[100%] my-8 bg-w1" />
     <AddBook />
