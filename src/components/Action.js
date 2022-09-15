446231add858dccfa7930dba6837df3d0ddfe7cb
binhussen <mhdhsna@gmail.com>
2022-09-15 12:22:30 +0300

    Modify @layer for linter

--- src/components/Action.js
+++ src/components/Action.js
@@ -2,15 +2,24 @@
 
 const Action = () => (
   <div className="flex items-center mt-5">
-    <button type="button" className="btn">
+    <button
+      type="button"
+      className="font-serif cursor-pointer font-light text-sm text-secondary"
+    >
       Comment
     </button>
     <div className="mx-4 bg-w1 h-6 w-0.5" />
-    <button type="button" className="btn">
+    <button
+      type="button"
+      className="font-serif cursor-pointer font-light text-sm text-secondary"
+    >
       Remove
     </button>
     <div className="mx-4 bg-w1 h-6 w-0.5" />
-    <button type="button" className="btn">
+    <button
+      type="button"
+      className="font-serif cursor-pointer font-light text-sm text-secondary"
+    >
       Edit
     </button>
   </div>
