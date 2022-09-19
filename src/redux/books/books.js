a40835f306b557e4e435afe2052b2525c1e8cd9f
binhussen <mhdhsna@gmail.com>
2022-09-19 21:12:53 +0300

    Add initial book data

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,16 +1,42 @@
 /** @format */
-export const CREATE_BOOK = 'bookstore/books/CREATE';
-export const REMOVE_BOOK = 'bookstore/books/REMOVE';
+import { v4 as uuidv4 } from 'uuid';
 
-export default (state = [], action) => {
-  switch (action.type) {
-    case CREATE_BOOK:
-      return [...state, action.book];
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.book.id);
-    default:
-      return state;
-  }
+const CREATE_BOOK = 'bookstore/books/CREATE_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+
+const initialBooks = [
+	{
+		id: uuidv4(),
+		title: 'The Hunger Games',
+		category: 'Action',
+		author: 'Suzanne Collins',
+		progress: { percent: 64, chapter: 17 },
+	},
+	{
+		id: uuidv4(),
+		title: 'Dune',
+		category: 'Sci-Fi',
+		author: 'Frank Herbert',
+		progress: { percent: 8, chapter: 3 },
+	},
+	{
+		id: uuidv4(),
+		title: 'Capital in the Twenty-First Century',
+		category: 'Economy',
+		author: 'Suzanne Collins',
+		progress: { percent: 0, chapter: 1 },
+	},
+];
+
+export default (state = initialBooks, action) => {
+	switch (action.type) {
+		case CREATE_BOOK:
+			return [...state, action.book];
+		case REMOVE_BOOK:
+			return state.filter((book) => book.id !== action.id);
+		default:
+			return state;
+	}
 };
 
 export const createBook = (book) => ({
@@ -18,7 +44,7 @@ export const createBook = (book) => ({
   book,
 });
 
-export const removeBook = (book) => ({
-  type: REMOVE_BOOK,
-  book,
+export const removeBook = (id) => ({
+	type: REMOVE_BOOK,
+	id,
 });
