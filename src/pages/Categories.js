d55cb497ade2ca9412bf41632820a45358bb0d67
binhussen <mhdhsna@gmail.com>
2022-09-20 14:43:56 +0300

    Make it Responsive

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -15,7 +15,7 @@ const Categories = () => {
     <div className="p-[5%] bg-w2">
       <button
         type="button"
-        className=" text-white font-bold font-serif px-6 py-2 rounded-md bg-primary cursor-pointer w-[20%]"
+        className=" text-white font-bold font-serif px-6 py-2 rounded-md bg-primary cursor-pointer w-[20%] items-center"
         onClick={handleStatus}
       >
         Check status
